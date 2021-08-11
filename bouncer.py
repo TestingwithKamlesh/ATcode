84e2fed870173f846bc229590b3b48ed67dbd2af
testinghero <kamlesh.ku1293@gmail.com>
2021-08-11 22:04:46 +0530

    first commit

---
+++ bouncer.py
@@ -0,0 +1,9 @@
+age = input("your age: ")
+age = int(age)
+if age >= 18 and age < 21:
+	print("you can wear wrist bands")
+elif xage >= 21:
+	print("you can have drinks too")
+else:
+	print("sorry, you are too young")
+	
